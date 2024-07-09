bb33918c2e6693fc1401974ea60bfde8a569df70
Brian Ratemo Magucha <brianratemom@gmail.com>
2024-07-09 22:04:49 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(name,email){
+    const url='http://localhost:3000/users'
+    const configObj={
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json',
+            'Accept':'application/json'
+        },
+        body:JSON.stringify({
+            name:name,
+            email:email
+        })
+    }
+    return fetch(url,configObj)
+    .then(response=> response.json())
+    .then(data=>{
+        document.body.innerHTML+= `<p>${data.id}</p>`
+        return data
+    })
+    .catch(error=>{
+        document.body.innerHTML+=`<p>${error.message}</p>`
+        return error
+    })
+}
